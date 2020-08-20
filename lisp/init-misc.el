584b221b285abbd834d5088d51ec108baa8dc141
Andrei <queyenth@gmail.com>
2020-08-20 15:19:52 +0600

    Projectile and Ivy

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -3,4 +3,16 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq create-lockfiles nil)
 
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1))
+
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
 (provide 'init-misc)
