b453fdcdab429b31080ca83d16b67887421452fc
Andrei <queyenth@gmail.com>
2020-08-23 12:11:23 +0600

    Add flycheck and lsp mode

--- lisp/init-code.el
+++ lisp/init-code.el
@@ -1,5 +1,26 @@
 (require 'init-elpa)
 
-;; Check LSP mode for eamcs
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((php-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :config
+  (use-package lsp-ui
+    :ensure t
+    :commands lsp-ui-mode)
+
+  (use-package lsp-ivy
+    :ensure t
+    :commands lsp-ivy-workspace-symbol)
+
+  (use-package which-key
+    :ensure t
+    :config
+    (which-key-mode)))
 
 (provide 'init-code)
