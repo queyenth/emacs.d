341861854cbca3be5b10eaecce32dcf69924783f
Andrei Queyenth <queyenth@gmail.com>
2020-08-27 19:40:14 +0600

    Some temp changes to org

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,20 @@
 (require 'init-elpa)
 
 (use-package org
-  :ensure t)
+  :ensure t
+  :config
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+  (setq org-log-reschedule 'note)
+  (setq org-refile-allow-creating-parent-nodes 'confirm)
+  (setq org-refile-targets '(org-agenda-files :level . 1))
+  (setq org-refile-use-outline-path 'file)
+  (setq org-indent-indentation-per-level 1)
+  (setq org-adapt-indentation nil)
+  (setq org-hide-leading-stars 't)
+  (setq org-hide-emphasis-markers t)
+  (setq org-cycle-separator-lines 1)
+  (setq org-blank-before-new-entry '((heading . nil) (plain-list-item . nil)))
+  (setq org-agenda-files '("~/Default/org/life.org")))
 
 (provide 'init-org)
