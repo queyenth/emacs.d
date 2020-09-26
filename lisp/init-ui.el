43608331fee013bfa326dc61e55b5f795b57d280
Andrei <queyenth@gmail.com>
2020-09-27 02:45:45 +0600

    Misc stuff for UI

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -6,6 +6,8 @@
   (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
+(tooltip-mode -1)
+(set-fringe-mode 10)
 
 (setq
   x-select-enable-clipboard t
@@ -14,30 +16,35 @@
   apropos-do-all t
   mouse-yank-at-point t)
 
-(global-display-line-numbers-mode)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;(global-display-line-numbers-mode)
+(column-number-mode)
+(use-package linum-relative
+  :init
+  (setq linum-relative-backend 'display-line-numbers-mode)
+  :config
+  (linum-relative-global-mode))
 
 (blink-cursor-mode 0)
 (setq-default cursor-type 'bar)
 (setq ring-bell-function 'ignore)
 
-(use-package base16-theme
-             :config
-             (load-theme 'base16-tomorrow t))
-
-(use-package minions
-  :config
-  (minions-mode 1))
-
-(use-package smart-mode-line
+(use-package doom-modeline
   :init
-  (setq sml/theme 'light)
-  (setq sml/no-confirm-load-theme t)
+  (doom-modeline-mode 1))
+
+;; (use-package gruvbox-theme
+;;              :config
+;;              (load-theme 'gruvbox t))
+
+(use-package doom-themes
   :config
-  (sml/setup))
+  (load-theme 'doom-gruvbox t))
 
-(use-package page-break-lines
-
-(use-package all-the-icons
+(use-package page-break-lines)
+(use-package all-the-icons)
 
 (use-package dashboard
   :init
@@ -54,6 +61,6 @@
 (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
 
 ;;(set-default-font "Hack-10")
-(set-face-attribute 'default t :font "Hack-10")
+(set-face-attribute 'default nil :font "Fira Mono for Powerline" :height 100)
 
 (provide 'init-ui)
