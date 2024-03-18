0368abe8fa0313473b5796f011a83d0d2cf588ef
Andrei <queyenth@gmail.com>
2024-03-18 06:39:16 +0600

    Don't need this clipboard thing; I live in emacs

--- init.el
+++ init.el
@@ -69,9 +69,6 @@
 (setq show-trailing-whitespace t)
 
 (setq
- x-select-enable-clipboard t
- x-select-enable-primary t
- save-interprogram-paste-before-kill t
  apropos-do-all t
  mouse-yank-at-point t)
 (setq-default cursor-type 'bar)
